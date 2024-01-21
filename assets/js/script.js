37c36ce2fb11643b9b255408c8517add8f93aabf
Holly Coloma <holly.life.15@gmail.com>
2024-01-21 23:00:18 +0000

    Add function to login form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,16 @@
 //This function will run when login button is clicked
-//set credentials to log in and validate
-    let uname = "admin";
-    let pword = "pass1234"
+    function logMein() {
+        //set credentials to log in
+              
+        let username = document.getElementById('username').value;
+        let password = document.getElementById('password').value;
+        let uname = "admin";
+        let pword = "pass1234";
 
-    //validate username and password
-    function logMein(username,password) {
-        if (username === uname && password === pword) {
+        if (!username || !password) {
+            alert('Please enter both username and password.');
+            return;
+        } else if (username === uname && password === pword) {
             alert('Login successfull!');
             //Redirect to register.html. Enter code here 
 
