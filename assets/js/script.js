6006b5036e9616df54dd66e0d07aeb0f7b06ae04
Holly Coloma <holly.life.15@gmail.com>
2024-01-21 22:19:57 +0000

    Add style and function to login page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
-let salary = document.getElementById('salaryYear');
+//This function will run when login button is clicked
+//set credentials to log in and validate
+    let uname = "admin";
+    let pword = "pass1234"
 
-console.log(salary.value);
+    //validate username and password
+    function logMein(username,password) {
+        if (username === uname && password === pword) {
+            alert('Login successfull!');
+            //Redirect to register.html. Enter code here 
+
+        } else {
+            alert('Invalid username or password. Please try again.');
+        }
+    
+
+
+}
